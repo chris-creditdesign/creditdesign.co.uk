188fd74889b875214353fc7d9c290c917c1122e9
Chris Ryan <creditdesign@gmail.com>
2021-12-24 14:05:28 +0000

    Convert PortfolioSection to grid

--- src/lib/PortfolioSection/index.svelte
+++ src/lib/PortfolioSection/index.svelte
@@ -1,18 +1,41 @@
 <script>
+	import { Center, Stack, GridList } from 'creditdesign-svelte-components';
 	import { CardSideImage, CardSideImageBox } from 'nature-immersive-svelte-components';
 
 	export let articles;
 </script>
 
-{#each articles as cardData, i}
-	{#if i % 2}
-		<CardSideImage headerLevel="none" headlineFontSize="big-1" {cardData} sidebarOnLeft={i % 2} />
-	{:else}
-		<CardSideImageBox
-			headerLevel="none"
-			headlineFontSize="big-1"
-			{cardData}
-			sidebarOnLeft={i % 2}
-		/>
-	{/if}
-{/each}
+<Center centerMeasure="140ch">
+	<Stack stackSpace="var(--s-1)">
+		<h2 class="border-above">Portfolio</h2>
+
+		<p>These are some of my favourite projects from the last few years:</p>
+
+		<GridList gridSpace="var(--s4)">
+			{#each articles as cardData, i}
+				{#if i === 0 || i === 3}
+					<li>
+						<CardSideImageBox
+							headerLevel="none"
+							headlineFontSize="big-1"
+							{cardData}
+							imageWidth="20ch"
+							sidebarSpace="var(--s1)"
+						/>
+					</li>
+				{:else}
+					<li>
+						<CardSideImage
+							headerLevel="none"
+							headlineFontSize="big-1"
+							{cardData}
+							imageWidth="20ch"
+							sidebarSpace="var(--s1)"
+							imageOnLeft={false}
+						/>
+					</li>
+				{/if}
+			{/each}
+		</GridList>
+	</Stack>
+</Center>
